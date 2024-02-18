509142eb36412b655dcb9241c142571f6cb3cbe4
EduRamosLima <edr.ramos.lima@gmail.com>
2024-02-18 20:56:33 -0300

    Add bottom card component

---
+++ src/app/components/bottom-card/bottom-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BottomCardComponent } from './bottom-card.component';
+
+describe('BottomCardComponent', () => {
+  let component: BottomCardComponent;
+  let fixture: ComponentFixture<BottomCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BottomCardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BottomCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
